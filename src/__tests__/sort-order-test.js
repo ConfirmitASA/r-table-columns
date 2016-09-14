3e1f35d74ca2dce55d6895f615eba3d4f384a3a4
jahglow <jahglow@gmail.com>
2016-09-14 17:16:56 +0300

    sort order tests and sort order refactored

--- src/__tests__/sort-order-test.js
+++ src/__tests__/sort-order-test.js
@@ -1,30 +1,119 @@
 import SortOrder from '../sort-order';
 import TableColumns from '../table-columns';
 
-const columns = [{
-    index:0,
-    title: "Column1",
-    colSpan:1,
-    cell: document.createElement('td'),
-    refCell: document.createElement('td')
-  },
-  {
-    index:1,
-    title: "Column2",
-    colSpan:2,
-    cell: document.createElement('td'),
-    refCell: document.createElement('td')
-  },
-  {
-    index:2,
-    title: "Column3",
-    colSpan:1,
-    cell: document.createElement('td'),
-    refCell: document.createElement('td')
-  }];
 
-describe('SortOrder.getCell', () => {
-  it('parameter should not be null', () => {
-    expect(()=>SortOrder.getCell()).toThrow();
+describe('SortOrder', () => {
+  var columns, spy, so = {column:1, direction: 'asc'};
+  beforeEach(()=>{
+    jasmine.getFixtures().fixturesPath = 'base/src/__tests__/fixtures';
+    loadFixtures('table-nested-headers.html');
+    columns = new TableColumns({source:$j('#confirmit_agg_table')[0],refSource:$j('#confirmit_agg_table')[0]});
+    spy={
+      callback:function(){}
+    };
+    spyOn(spy,'callback');
+
+  });
+  describe('SortOrder.constructor', () => {
+    it('should throw if columns are undefined', () => {
+      expect(()=>new SortOrder()).toThrow();
+    });
+    it('should throw if columns are null', () => {
+      expect(()=>new SortOrder(null)).toThrow();
+    });
+    it('should have columns property', () => {
+      let s = new SortOrder(columns);
+      expect(s.columns).toBeDefined();
+      expect(s.columns).toEqual(columns);
+    });
+    it('should initialize with a default sorting and call a sorting callback once', () => {
+      let s = new SortOrder(columns, spy.callback ,[so]);
+      expect(s.sortOrder.length).toEqual(1);
+      expect(s.sortOrder[0].column).toEqual(1);
+      expect(spy.callback).toHaveBeenCalled();
+      expect(spy.callback).toHaveBeenCalledTimes(1);
+    });
+    it('should initialize with a default sorting and call a default sorting callback once, ignoring the wrong type of callback', () => {
+      let s = new SortOrder(columns, {} ,[so]);
+      expect(s.sortOrder.length).toEqual(1);
+      expect(s.sortOrder[0].column).toEqual(1);
+      expect(spy.callback).not.toHaveBeenCalled();
+    });
+    it('should add a .sorted.desc class to the 1st column', () => {
+      let sortOrder = new SortOrder(columns, spy.callback ,[so]);
+      let cells = sortOrder.getCell(1);
+      expect($j(cells[0])).toBeMatchedBy('.sorted');
+      expect($j(cells[1])).toBeMatchedBy('.sorted');
+      expect($j(cells[0])).toBeMatchedBy('.asc');
+      expect($j(cells[1])).toBeMatchedBy('.asc');
+    });
+  });
+  describe('SortOrder.add', () => {
+    it('should add a column to the existing setup but not trigger callback', () => {
+      let sortOrder= new SortOrder(columns,spy.callback,[so]);
+      expect(sortOrder.sortOrder.length).toEqual(1);
+      expect(spy.callback).toHaveBeenCalledTimes(1);
+      sortOrder.add({column:2,direction:'desc'});
+      expect(sortOrder.sortOrder.length).toEqual(2);
+      expect(sortOrder.sortOrder[1].column).toEqual(2);
+      expect(spy.callback).toHaveBeenCalledTimes(1);
+      let cells = sortOrder.getCell(2);
+      expect($j(cells[0])).toBeMatchedBy('.sorted');
+      expect($j(cells[1])).toBeMatchedBy('.sorted');
+      expect($j(cells[0])).toBeMatchedBy('.desc');
+      expect($j(cells[1])).toBeMatchedBy('.desc');
+
+    });
+  });
+  describe('SortOrder.getCell', () => {
+    it('should throw if columnIndex is not passed', () => {
+      let sortOrder= new SortOrder(columns,spy.callback,[so]);
+      expect(()=>sortOrder.getCell()).toThrow();
+    });
+    it('should return only one column', () => {
+      let sortOrder= new SortOrder(new TableColumns({source:$j('#confirmit_agg_table')[0]}),spy.callback,[so]);
+      expect(sortOrder.getCell(1).length).toEqual(1);
+    });
+  });
+  describe('SortOrder.replace', () => {
+    it('should replace sortOrder[0] with a new object, call a callback and recompute classes', () => {
+      let sortOrder= new SortOrder(columns,spy.callback,[so]);
+      expect(spy.callback).toHaveBeenCalledTimes(1);
+      let cells = sortOrder.getCell(1);
+      expect($j(cells[0])).toBeMatchedBy('.sorted');
+      expect($j(cells[1])).toBeMatchedBy('.sorted');
+      expect($j(cells[0])).toBeMatchedBy('.asc');
+      expect($j(cells[1])).toBeMatchedBy('.asc');
+
+      sortOrder.replace({column:2,direction:'desc'});
+      expect(sortOrder.sortOrder.length).toEqual(1);
+      expect(sortOrder.sortOrder[0].column).toEqual(2);
+      expect(spy.callback).toHaveBeenCalledTimes(2);
+      let cells1 = sortOrder.getCell(2);
+      expect($j(cells1[0])).toBeMatchedBy('.sorted');
+      expect($j(cells1[1])).toBeMatchedBy('.sorted');
+      expect($j(cells1[0])).toBeMatchedBy('.desc');
+      expect($j(cells1[1])).toBeMatchedBy('.desc');
+      expect($j(cells[0])).not.toBeMatchedBy('.sorted');
+      expect($j(cells[1])).not.toBeMatchedBy('.sorted');
+      expect($j(cells[0])).not.toBeMatchedBy('.asc');
+      expect($j(cells[1])).not.toBeMatchedBy('.asc');
+    });
+    it('should add a new object to sortOrder which is empty, call a callback and compute classes', () => {
+      let sortOrder= new SortOrder(columns,spy.callback);
+      expect(sortOrder.sortOrder.length).toEqual(0);
+
+      expect(spy.callback).not.toHaveBeenCalled();
+
+      sortOrder.replace({column:2,direction:'desc'});
+      expect(sortOrder.sortOrder.length).toEqual(1);
+      expect(sortOrder.sortOrder[0].column).toEqual(2);
+      expect(spy.callback).toHaveBeenCalledTimes(1);
+      let cells1 = sortOrder.getCell(2);
+      expect($j(cells1[0])).toBeMatchedBy('.sorted');
+      expect($j(cells1[1])).toBeMatchedBy('.sorted');
+      expect($j(cells1[0])).toBeMatchedBy('.desc');
+      expect($j(cells1[1])).toBeMatchedBy('.desc');
+    });
   });
 });
--- src/sort-order.js
+++ src/sort-order.js
@@ -1,30 +1,33 @@
 class SortOrder {
   /**
-   * Creates a `sortOrder` array with static functions `add`,`remove`,`replace`
+   * Creates a `sortOrder` array
    * @param {Object} columns - an array of columns from {@link TableColumns}
    * @param sortCallback - function that performs sorting based on the `sortOrder`
-   * @param {Object} defaultSorting - an array of objects that specify default sorting
+   * @param {Object} [defaultSorting] - an array of objects that specify default sorting
    * @param {Number} defaultSorting.column - column index
    * @param {String} defaultSorting.direction - sort direction (`asc`|`desc`)
    * @return {Array}
    * */
-
-  constructor(columns, sortCallback, defaultSorting){
+  constructor(columns, sortCallback, defaultSorting=[]){
     this.sortOrder = [];
-    this.columns = columns;
-    if(defaultSorting.length>0){
-      defaultSorting.forEach(item=>this.constructor.add(item));
+    if(typeof columns != undefined && columns != null){
+      this.columns = columns;
+    } else {
+      throw new TypeError('SortOrder: columns must be specified');
     }
     this.constructor.sort = sortCallback && typeof sortCallback === 'function'? sortCallback : function(){};
-
-    return this.sortOrder;
+    if(defaultSorting.length>0){
+      defaultSorting.forEach(item=>this.add(item));
+      this.constructor.sort();
+    }
   }
 
   /**
-   * Returns an array containing a cell from the table and a reference cell from the floating header if any
+   * Returns an array containing a `cell` from the table and a reference cell (`refCell`) from the floating header if any
    * @param {!Number} columnIndex - index of the column from the array of columns from {@link TableColumns}
+   * @return {[{cell:HTMLTableCellElement, ?refCell:HTMLTableCellElement}]}
    * */
-  static getCell(columnIndex){
+  getCell(columnIndex){
     if(typeof columnIndex != 'undefined' && columnIndex!=null){
       let cells = [];
       if(this.columns[columnIndex].cell){cells.push(this.columns[columnIndex].cell)}
@@ -42,13 +45,13 @@ class SortOrder {
    * @param {String} obj.direction - sort direction (`asc`|`desc`)
    * */
 
-  static add (obj){
-    this.constructor.getCell(obj.column).forEach(cell=>{
-      if(!cell.classList.contains('sorted')){ // this column is not sorted, there might be others that are.
+  add (obj){
+    this.getCell(obj.column).forEach(cell=>{
+      //if(!cell.classList.contains('sorted')){ // this column is not sorted, there might be others that are.
         ['sorted',obj.direction].forEach(className=>cell.classList.add(className));
-      } else { //swaps sorting from asc to desc
-        ['asc','desc'].forEach(className=>cell.classList.toggle(className));
-      }
+      //} else { //swaps sorting from asc to desc
+      //  ['asc','desc'].forEach(className=>cell.classList.toggle(className));
+      //}
     });
     this.sortOrder.push(obj);
   }
@@ -58,9 +61,9 @@ class SortOrder {
    * @param {Number} column - column index as reference to the item to be removed.
    * @param {Number} index - index of item in `sortOrder` array to be removed
    * */
-  static remove (column,index){
+  remove (column,index){
     ['sorted','asc','desc'].forEach(className=>{
-      this.constructor.getCell(column).forEach(cell=>cell.classList.remove(className))
+      this.getCell(column).forEach(cell=>cell.classList.remove(className))
     });
     this.sortOrder.splice(index,1);
   };
@@ -71,14 +74,15 @@ class SortOrder {
    * @param {Number} obj.column - column index
    * @param {String} obj.direction - sort direction (`asc`|`desc`)
    * */
-  static replace (obj){
+  replace (obj){
     if(this.sortOrder.length>0){
       this.sortOrder.forEach((item,index)=>{
-        this.constructor.remove(item.column,index);
+        this.remove(item.column,index);
       });
     }
-    this.constructor.add(obj);
+    this.add(obj);
     this.constructor.sort();
   };
+  //static sort(){}
 }
 export default SortOrder;
