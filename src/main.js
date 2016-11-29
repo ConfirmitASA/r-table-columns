a9a86c210f9b6aeeb8dc58057e0999fa31d7f478
jahglow <jahglow@gmail.com>
2016-11-29 10:27:52 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * Created by IvanP on 07.09.2016.
  */
 import TableColumns from "./table-columns";
-import ReportalBase from "r-reporal-base/src/reportal-base";
+import ReportalBase from "r-reportal-base";
 
 window.Reportal = window.Reportal || {};
 ReportalBase.mixin(window.Reportal,{
